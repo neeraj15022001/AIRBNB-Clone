a25c1f97b9d1edcb3edc1bf57de25aae1ebf1a61
neeraj15022001 <gneeraj32595@gmail.com>
2021-07-04 00:49:58 +0530

    added tailwind

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Airbnb under construction</h1>
+    <div className="bg-red-700 text-white h-screen flex items-center justify-center">
+      <h1 className="text-7xl py-6 text-center"><span className="text-red-900 text-9xl">AIRBNB</span> under construction</h1>
     </div>
   );
 }
