f68ca03c0aa2ce4c84e639ae965e88e9216dc520
neeraj15022001 <gneeraj32595@gmail.com>
2021-07-04 23:16:15 +0530

    navbar and home

---
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  SearchIcon,
+  HeartIcon,
+  UserCircleIcon,
+} from "@heroicons/react/outline";
+
+function Navbar() {
+  return (
+    <div className="py-3 flex items-center justify-center border-t fixed bottom-0 left-0 right-0">
+      <div className="w-1/2 flex items-center justify-between">
+        <div className="flex items-center justify-center flex-col">
+          <SearchIcon className="h-7 text-red-600" />
+          <p className="text-xs capitalize">explore</p>
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <HeartIcon className="h-7 text-gray-400" />
+          <p className="text-xs text-gray-400 capitalize">wishlist</p>
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <UserCircleIcon className="h-7 text-gray-400" />
+          <p className="text-xs text-gray-400 capitalize">Log in</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/Components/Pages/Home/Body/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,38 @@
+import { SearchIcon } from "@heroicons/react/outline";
+import React from "react";
+import styled from "styled-components";
+import hero from "./Resources/hero.jpg";
+
+function HeroSection() {
+  const Wrapper = styled.div`
+    background-image: url(${(props) => props.img});
+    background-size: contain;
+    background-repeat: repeat-x;
+  `;
+  return (
+    <Wrapper img={hero} className="h-2/3 p-4 relative">
+      <div className="w-full bg-white flex items-center justify-center py-3 rounded-full">
+        <SearchIcon className="h-6 text-red-600 mr-2" />
+        <input
+          type="text"
+          name="search"
+          placeholder="Where are you going?"
+          className="outline-none"
+        />
+      </div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center flex-col">
+        <p className="font-medium text-center mb-4">
+          Not sure where to go? <br />{" "}
+          <span className="capitalize">perfect.</span>
+        </p>
+        <button className="bg-white w-auto h-auto rounded-full transform scale-100 py-3 px-6 shadow-md transition duration-500 ease-in-out  hover:scale-90 shadow-lg">
+          <span className="rounded-full font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-pink-600 text-lg">
+            I'm flexible
+          </span>
+        </button>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default HeroSection;
