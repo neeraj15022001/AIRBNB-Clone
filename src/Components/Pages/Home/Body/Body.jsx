652111caa56bed350ec29b1c36c1a576fd1c0775
neeraj15022001 <gneeraj32595@gmail.com>
2021-07-07 16:31:13 +0530

    added responsiveness

--- src/Components/Pages/Home/Body/Body.jsx
+++ src/Components/Pages/Home/Body/Body.jsx
@@ -4,7 +4,7 @@ import HeroSection from "./Components/HeroSection/HeroSection";
 import Nearby from "./Components/Nearby/Nearby";
 import Live from "./Components/Live/Live";
 import Discover from "./Components/Discover/Discover";
-import { bg1, bg2 } from "./Images.js";
+import { bg1, bg2, bglg1, bglg2 } from "./Images.js";
 import SectionCards from "./Components/SectionCards/SectionCards";
 import Footer from "./Components/Footer/Footer";
 
@@ -16,7 +16,7 @@ function Body() {
       <Nearby />
       <Live />
       <SectionCards
-        bg={bg1}
+        bg={window.innerWidth < 1024 ?  bg1 : bglg1}
         title="the greatest outdoors"
         titleColorClass="text-black"
         subtitle="Wishlists curated by Airbnb."
@@ -27,7 +27,7 @@ function Body() {
       />
       <Discover />
       <SectionCards
-        bg={bg2}
+        bg={window.innerWidth < 1024 ?  bg2 : bglg2}
         title="try hosting"
         titleColorClass="text-white"
         subtitle="Earn extra income and unlock new oppurtunities by sharing your space."
